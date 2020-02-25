3984e1bf4da10ae6b8b7cbdade988813efe811d6
Raul Menezes <raul@appscatter.com>
2020-02-25 11:17:03 +0000

    Base Project Commit

---
+++ src/main/java/raul/springframework/fruitshop/FruitShopApplication.java
@@ -0,0 +1,13 @@
+package raul.springframework.fruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitShopApplication.class, args);
+    }
+
+}
