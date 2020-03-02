72741746ecd314cf66d49caab2610ad87e8384ac
Raul Menezes <raul@appscatter.com>
2020-03-02 18:23:42 +0000

    Customizing Swagger with Meta Data

--- src/main/java/raul/springframework/fruitshop/config/SwaggerConfig.java
+++ src/main/java/raul/springframework/fruitshop/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +21,24 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Raul Menezes", "https://raulmenezes.com",
+                "");
+
+        return new ApiInfo(
+                "Fruit Shop API",
+                "Fruit Shop Documentation",
+                "1.0",
+                "Terms of Service: URL",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 }
